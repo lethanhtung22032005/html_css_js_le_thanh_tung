8d786322c6ac1beb83cfb3ec9152fa4ecb5a529a
lethanhtung22032005 <lethanhtungdeptrai@gmail.com>
2025-02-27 22:25:36 +0700

    add

---
+++ Session17/baitap/baitap11.js
@@ -0,0 +1,36 @@
+// Sử dụng hàm Math.abs()  
+let absValue1 = Math.abs(-5.5);
+console.log(absValue1); // Kết quả: 5.5  
+// Math.abs() trả về giá trị tuyệt đối của số. Với -5.5, kết quả là 5.5.  
+
+let absValue2 = Math.abs(10);
+console.log(absValue2); // Kết quả: 10  
+// Với 10, giá trị tuyệt đối cũng là chính nó, vì vậy kết quả là 10.  
+
+// Sử dụng hàm Math.ceil()  
+let ceilValue1 = Math.ceil(4.4);
+console.log(ceilValue1); // Kết quả: 5  
+// Math.ceil() làm tròn lên tới số nguyên gần nhất. 4.4 được làm tròn lên thành 5.  
+
+let ceilValue2 = Math.ceil(5.8);
+console.log(ceilValue2); // Kết quả: 6  
+// Tương tự, 5.8 cũng được làm tròn lên thành 6.  
+
+// Sử dụng hàm Math.floor()  
+let floorValue1 = Math.floor(5.7);
+console.log(floorValue1); // Kết quả: 5  
+// Math.floor() làm tròn xuống tới số nguyên gần nhất. 5.7 được làm tròn xuống thành 5.  
+
+let floorValue2 = Math.floor(6.1);
+console.log(floorValue2); // Kết quả: 6  
+// Với 6.1, Math.floor() cũng trả về 6, vì nó làm tròn xuống tới số nguyên.  
+
+
+// Sử dụng hàm Math.round()  
+let roundValue1 = Math.round(10.1);
+console.log(roundValue1); // Kết quả: 10  
+// Math.round() làm tròn tới số nguyên gần nhất. 10.1 được làm tròn xuống thành 10.  
+
+let roundValue2 = Math.round(20.6);
+console.log(roundValue2); // Kết quả: 21  
+// 20.6 được làm tròn lên thành 21, vì nó nằm gần hơn với 21 hơn là 20.  
