196e667667484d47f1bcc3df1aee267657977952
lethanhtung22032005 <lethanhtungdeptrai@gmail.com>
2025-03-04 00:12:39 +0700

    add

--- Session19/baitap/baitap2.js
+++ Session19/baitap/baitap2.js
@@ -1,22 +1,5 @@
-let inputChar = prompt("Nhập một ký tự:");
-let doDai = inputChar.length;
-if (doDai > 1) {
-    if (inputChar.indexOf(0 && 1 && 2 && 3 && 4 && 5 && 6 && 7 && 8 && 9) == -1) {
-        console.log(inputChar);
-        document.write("khong phai la mot chu");
-    } else {
-        document.write("la mot chu");
-    }
+const char = prompt("Xin mời nhập ký tự");
+if (/^[a-zA-Z]$/.test(char)) {
+    alert(`${char} là chữ cái`);
 } else {
-    document.write("khong phai chu");
-}
-
-// if(doDai>1){
-//    document.write(inputChar + " không phải là một chữ cái.");
-// }else{
-//     if ((inputChar >= 'a' && inputChar <= 'z') || (inputChar >= 'A' && inputChar <= 'Z')) {
-//        document.write(inputChar + " là một chữ cái.");
-//     } else {
-//        document.write(inputChar + " không phải là một chữ cái.");
-//     }
-// }
+    alert(`Ký tự ${char} không phải chữ cái`);
